05e27c0b35e0d956c52b412b7857e7c242f93227
Dayanapnf <71513260+Dayanapnf@users.noreply.github.com>
2024-03-07 17:09:31 -0300

    atv02

--- react_completo/src/App.js
+++ react_completo/src/App.js
@@ -5,9 +5,22 @@ import Header from './components/Atividade02/Header';
 import Home from './components/Atividade02/Home';
 import AtvMap from './components/AtvMap';
 import Produtos from './components/Atividade02/Produtos';
+import Produto from './components/Atividade03/Produto';
+import { useState } from 'react';
 
 function App() {
   const { pathname } = window.location;
+  const [dados, setDados] = useState(null);
+  const [carregando, setCarregando] = useState(null);
+  async function handleClick(event) {
+    setCarregando(true);
+    const response = await fetch(
+      `https://ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`,
+    );
+    const json = await response.json();
+    setDados(json);
+    setCarregando(false);
+  }
 
   let Component;
   if (pathname === '/produtos') {
@@ -20,8 +33,13 @@ function App() {
       {/* <Atividade /> */}
       {/* <AtvMap /> */}
       {/* <Atividade01 /> */}
-      <Header />
-      <Component />
+      {/* <Header /> */}
+      {/* <Component /> */}
+      <button onClick={handleClick}>smartphone</button>
+      <button onClick={handleClick}>tablet</button>
+      <button onClick={handleClick}>notebook</button>
+      {carregando && <p>Carregando...</p>}
+      {!carregando && dados && <Produto dados={dados} />}
     </div>
   );
 }
